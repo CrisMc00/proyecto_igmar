ab245af438fd079a73ccbb563ce12d2814fd0fa5
cristianmatachairez <cristianmc1609@gmail.com>
2026-03-10 16:35:33 -0600

    se creo la migracion

--- database/migrations/2026_03_10_174830_add_role_to_users_table.php
+++ database/migrations/2026_03_10_174830_add_role_to_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->string('role')->default('invitado')->after('email');
         });
     }
 
@@ -23,6 +24,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
+            $table->dropColumn('role');
         });
     }
 };
