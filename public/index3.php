7f7fe34ba77b74893e8b0e2115db67068c869f5a
cristianmatachairez <cristianmc1609@gmail.com>
2026-03-10 20:50:11 -0600

    asdasd

--- public/index3.php
+++ public/index3.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-echo "Aqui es el puerto 8000";
+echo "Aqui es el puerto 8080";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
