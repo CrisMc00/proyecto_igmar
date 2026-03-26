7df0fe9a316bdca51f877a8e7e7aaf79721399b2
cristianmatachairez <cristianmc1609@gmail.com>
2026-03-26 12:13:10 -0600

    se elimino vistas que no tiene funcion

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 */
 
+/*
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+*/
 
 Route::get('/setup-2fa', [TwoFactorController::class, 'showSetup'])->name('2fa.setup');
 Route::get('/verify-otp', [OtpController::class, 'show'])->name('otp.verify');
